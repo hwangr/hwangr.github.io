1218b12a526df77f26a155e391ec31a52c374154
hwangr <hwangr@oregonstate.edu>
2018-11-29 16:03:58 -0500

    sss

--- Resources/Util_files/PrinterLocations.js
+++ Resources/Util_files/PrinterLocations.js
@@ -5,10 +5,10 @@ var pLocs = {
 		"b_name": "Duderstadt",
 		"printers": {
 			"B1": [
-				{"p_name": "m-dc-B245A-bw-1", 		"floor": "B1", "top": "53%", "left": "43%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-B309A-bw-1a", 		"floor": "B1", "top": "47%", "left": "56.5%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-B309A-bw-1b", 		"floor": "B1", "top": "47%", "left": "56.5%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-b430-bwmulti-1", 	"floor": "B1", "top": "22.5%", "left": "61.25%", "color": "N", "multi": "Y"},
+				{"p_name": "m-dc-B245A-bw-1", 		"floor": "B1", "top": "67%", "left": "49%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-B309A-bw-1a", 		"floor": "B1", "top": "59%", "left": "64%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-B309A-bw-1b", 		"floor": "B1", "top": "59%", "left": "64%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-b430-bwmulti-1", 	"floor": "B1", "top": "30%", "left": "69.5%", "color": "N", "multi": "Y"},
 			],
 			"F1": [
 				{"p_name": "m-dc-1485-bwmulti-1", 	"floor": 1, "top": "55%", "left": "56%", "color": "N", "multi": "Y"},
@@ -28,15 +28,15 @@ var pLocs = {
 				{"p_name": "m-dc-2C42-bwmulti-1", 	"floor": 2, "top": "35%", "left": "75%", "color": "N", "multi": "Y"}
 			],
 			"F3": [
-				{"p_name": "m-dc-3300-bw-1a", 		"floor": 3, "top": "71%", "left": "59%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3300-bw-1b", 		"floor": 3, "top": "71%", "left": "59%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3370-bw-1a", 		"floor": 3, "top": "50.5%", "left": "72%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3370-bw-1b", 		"floor": 3, "top": "50.5%", "left": "72%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3400-bw-1a", 		"floor": 3, "top": "22.5%", "left": "71%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3400-bw-1b", 		"floor": 3, "top": "22.5%", "left": "71%", "color": "N", "multi": "N"},
-				{"p_name": "m-dc-3400-color-1", 	"floor": 3, "top": "22.5%", "left": "71%", "color": "Y", "multi": "N"},
-				{"p_name": "m-dc-3C33-bwmulti-1", 	"floor": 3, "top": "58%", "left": "65%", "color": "N", "multi": "Y"},
-				{"p_name": "m-dc-3400-color-1", 	"floor": 3, "top": "70%", "left": "47.5%", "color": "Y", "multi": "N"}
+				{"p_name": "m-dc-3300-bw-1a", 		"floor": 3, "top": "84%", "left": "63.5%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3300-bw-1b", 		"floor": 3, "top": "84%", "left": "63.5%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3370-bw-1a", 		"floor": 3, "top": "61%", "left": "78%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3370-bw-1b", 		"floor": 3, "top": "61%", "left": "78%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3400-bw-1a", 		"floor": 3, "top": "28.5%", "left": "76.5%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3400-bw-1b", 		"floor": 3, "top": "28.5%", "left": "76.5%", "color": "N", "multi": "N"},
+				{"p_name": "m-dc-3400-color-1", 	"floor": 3, "top": "28.5%", "left": "76.5%", "color": "Y", "multi": "N"},
+				{"p_name": "m-dc-3C33-bwmulti-1", 	"floor": 3, "top": "70%", "left": "70%", "color": "N", "multi": "Y"},
+				{"p_name": "m-dc-3400-color-1", 	"floor": 3, "top": "83%", "left": "51%", "color": "Y", "multi": "N"}
 			]	
 				
 			
